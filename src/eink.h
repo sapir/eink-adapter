e7796de46be811b9a0a6464598552b42d1c64676
Y. Sapir <yasapir@gmail.com>
2016-12-18 00:33:27 +0200

    Change bitmap definitions to accomodate different pixel bit sizes.

--- src/eink.h
+++ src/eink.h
@@ -13,28 +13,28 @@ bool eink_setup(void);
 void eink_power_on(void);
 void eink_power_off(void);
 
-#define WHITE false
-#define BLACK true
-typedef bool pixel_t;
-
-
+typedef int pixel_t;
 #define PIXEL_BIT_SIZE 1
+#define WHITE 0
+#define BLACK 1
+
+
+#define PIXEL_BITMASK ((1<<PIXEL_BIT_SIZE) - 1)
 #define PIXELS_PER_BYTE (8 / PIXEL_BIT_SIZE)
 #define MAX_BITMAP_ROW_SIZE (SCREEN_WIDTH / PIXELS_PER_BYTE)
 
 static inline pixel_t get_row_pixel(const uint8_t *row, int x) {
     const int byte_index = x / PIXELS_PER_BYTE;
     const int bit_index = 8 - ((x % PIXELS_PER_BYTE) + 1) * PIXEL_BIT_SIZE;
-    const uint8_t bitmask = ((1<<PIXEL_BIT_SIZE) - 1) << bit_index;
-    return (row[byte_index] & bitmask) ? BLACK : WHITE;
+    return (row[byte_index] >> bit_index) & PIXEL_BITMASK;
 }
 
 static inline void set_row_pixel(uint8_t *row, int x, pixel_t p) {
     const int byte_index = x / PIXELS_PER_BYTE;
     const int bit_index = 8 - ((x % PIXELS_PER_BYTE) + 1) * PIXEL_BIT_SIZE;
-    const uint8_t bitmask = ((1<<PIXEL_BIT_SIZE) - 1) << bit_index;
-    row[byte_index] =
-        (row[byte_index] & ~bitmask) | ((p == BLACK) ? bitmask : 0);
+    const uint8_t shifted_bitmask = PIXEL_BITMASK << bit_index;
+    const uint8_t shifted_pixel = (p & PIXEL_BITMASK) << bit_index;
+    row[byte_index] = (row[byte_index] & ~shifted_bitmask) | shifted_pixel;
 }
 
 
